1f76fbc8784e1a3c0dfde52c4e79fe8c4fd73cd9
Abdi Wali <abdiwali2737@gmail.com>
2026-05-27 17:41:30 -0600

    initialized express application

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 300
+app.listen(PORT, ()=>console.log(`server is running at ${PORT}`))
