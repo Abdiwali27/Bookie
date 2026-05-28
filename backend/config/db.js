080f086884464069af8bec54b39a632c94a4f2e6
Abdi Wali <abdiwali2737@gmail.com>
2026-05-27 20:53:31 -0600

    3.connect mangodb

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URi);
+
+    console.log(`MongoDb Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import bookRouter from './routes/book.routes.js'
+import { connectDB } from './config/db.js'
 dotenv.config()
 
 const app = express()
@@ -8,4 +9,6 @@ const app = express()
 app.use("/api/books", bookRouter)
 
 const PORT = process.env.PORT || 5000
+
+connectDB()
 app.listen(PORT, ()=>console.log(`server is running at ${PORT}`))
