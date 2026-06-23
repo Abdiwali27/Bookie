32b89e8ae652cdb24e625263553708d3f9604ba1
Abdi Wali <abdiwali2737@gmail.com>
2026-06-23 17:51:02 -0600

    update books

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -57,8 +57,32 @@ res.status(201).json(book);
     }
 }
 
-export const updateBook = (req, res) => {
-    res.send("update  Book")
+export const updateBook = async(req, res) => {
+     try {
+    const book = await Book.findById(req.params.id)
+
+    if (!book) {
+        return res.status(404).json({
+            message: "Book Not Found"
+        })
+    }
+
+    const updatedBook = await Book.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            returnDocument: "after",
+            runValidators: true
+        }
+    )
+
+    res.status(200).json(updatedBook)
+
+} catch (error) {
+    res.status(500).json({
+        message: error.message
+    })
+}
 }
 
 export const deleteBook = (req, res) => {
