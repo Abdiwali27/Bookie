37731bc4e9265359b7d7b5aef8586beb8e031fe5
Abdi Wali <abdiwali2737@gmail.com>
2026-06-26 10:19:51 -0600

    fetch data from API

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv' 
 import bookRouter from './routes/book.routes.js' 
 import { connectDB } from './config/db.js' 
+import cors from 'cors'
 dotenv.config() 
 const app = express() 
+app.use(cors())
 app.use(express.json());
 app.use("/api/books", bookRouter) 
 const PORT = process.env.PORT || 8000 
